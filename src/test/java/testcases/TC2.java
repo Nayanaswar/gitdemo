354be1c1649723ffef05dedbabf3bbde8d2cdde2
Nayan <nayanaswar18@gmail.com>
2023-10-23 10:50:36 +0530

    added m2 method

--- src/test/java/testcases/TC2.java
+++ src/test/java/testcases/TC2.java
@@ -5,9 +5,14 @@ public class TC2
 	public void m1() 
 	{
 		System.out.println("This is m1 method.");
+		
+		
 	}
 	
-	
+	public void m2() 
+	{
+		System.out.println("This m2 method..............");
+	}
  
 
 }
